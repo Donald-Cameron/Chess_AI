11b6874e3c7d680e7d51537ea2b3f169ce897a3f
friedrich <friedrich.loparev@protonmail.ch>
2023-06-05 22:16:11 +0000

    Create empty chess field and print it

--- ChessFieldNumbers.java
+++ ChessFieldNumbers.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class ChessFieldNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello");
+
+        int[][] chessField = createStartingField();
+        for (int row = 0; row < chessField.length; row++) {
+            System.out.println();
+            for (int column = 0; column < chessField.length; column++) {
+                System.out.print(chessField[row][column] + "  ");
+            }
+        }
+    }
+
+    static int[][] createStartingField() {
+        int[][] chessField = new int[8][8];
+        return chessField;
     }
 }
